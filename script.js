612f21d8b4a60ecb1fa4c92a8e3ae098e523b53a
Ronakp08 <ronakp6393@gmail.com>
2025-01-29 09:22:08 +0530

    final

--- script.js
+++ script.js
@@ -1,64 +1,58 @@
 function toDo() {
+  const addButton = document.getElementById("add-task-btn");
+  const resetButton = document.getElementById("reset-btn");
 
-    
-
-    document.addEventListener("DOMContentLoaded", function () {
-        toDo();
-    });
+  const todoTable = document.querySelector(".task-table"); 
 
 
-    const addButton = document.getElementById("add-task-btn");
-    const resetButton = document.getElementById("reset-btn");
-  
-    // const todoTable = document.getElementById("task-table");
-  
-    function addTask(e) {
-        e.preventDefault();
-    
+  function addTask(e) {
+      e.preventDefault();  
+
       const taskTitle = document.getElementById("task-name").value;
       const priorityInput = document.getElementById("priority").value;
       const deadlineInput = document.getElementById("deadline").value;
       const commentInput = document.getElementById("comment").value;
 
-      const todoTable = document.getElementById(".task-table");
-  
-      
       if (taskTitle === "" || deadlineInput === "" || commentInput === "") {
-        alert("Please fill all details...!");
-        return;
+          alert("Please fill all details...!");
+          return;
       }
-  
+
      
       const newRow = document.createElement("tr");
       newRow.innerHTML = `
-        <td>${taskTitle}</td>
-        <td>${priorityInput}</td>
-        <td>${deadlineInput}</td>
-        <td>${commentInput}</td>
-        <td><button class="delete-btn">Delete</button></td>
+          <td>${taskTitle}</td>
+          <td>${priorityInput}</td>
+          <td>${deadlineInput}</td>
+          <td>${commentInput}</td>
+          <td><button class="delete-btn">Delete</button></td>
       `;
-      
-     
+
+   
       todoTable.appendChild(newRow);
-      resetForm();
-    }
-  
-    function resetForm() {
+      resetForm(); 
+  }
+
+
+  function resetForm() {
       document.getElementById("task-name").value = "";
       document.getElementById("priority").value = "high"; 
       document.getElementById("deadline").value = "";
       document.getElementById("comment").value = "";
-    }
-  
-   
-    addButton.addEventListener("click", function () {
-      addTask();
-      
-    });
-  
-   
-    resetButton.addEventListener("click", function () {
-      resetForm();
-    });
   }
-  
+
+
+  addButton.addEventListener("click", function (e) {
+      addTask(e);  
+  });
+
+
+  resetButton.addEventListener("click", function () {
+      resetForm();
+  });
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  toDo();
+});
